ba420b3aa3d2704d054c31ad17fca8d652808f5c
Chinmay-lucid <chinmay.chaturvedi@rm.team.lucidgrowth.com>
2025-09-12 12:55:46 +0530

    fix

--- server.js
+++ server.js
@@ -12,7 +12,11 @@ const PORT = 3000;
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(express.static('public'));
 
 // Redis client setup
